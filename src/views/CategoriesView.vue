2b8b8c670c0d4b42aa9065b9138c1d92cea645bc
josephLSalgado <josephLSalgado@outlook.com>
2022-05-10 19:12:47 -0500

    add: CategoriesView

---
+++ src/views/CategoriesView.vue
@@ -0,0 +1,16 @@
+<script>
+import Categories from '@/components/Categories.vue';
+
+export default {
+    name: 'CategoriesView',
+    components: {
+        Categories
+    }
+}
+</script>
+
+<template>
+    <div class="categories-view">
+        <Categories/>
+    </div>
+</template>
