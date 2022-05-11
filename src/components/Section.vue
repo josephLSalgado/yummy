5546e8068eb4ce1f1362721d560cf841d099ce55
josephLSalgado <josephLSalgado@outlook.com>
2022-05-10 19:06:40 -0500

    add: Section components

---
+++ src/components/Section.vue
@@ -0,0 +1,40 @@
+<script>
+export default {
+    name: 'Section',
+    props: ['section', 'url']
+}
+</script>
+
+<template>
+    <!-- Llama la función cuando se entra al div y le enviamos la variable 
+        section -->
+    <div class="section" @click="$store.dispatch('textToSpeech', section)">
+        <a :href="url"><h1>{{ section }}</h1></a>
+    </div>
+</template>
+
+
+<style scoped>
+.section {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: 400px;
+    height: 80px;
+    background: #A5B452;
+    border-radius: 20px;
+}
+
+a {
+    text-decoration: none;
+    color: black;
+}
+
+h1 {
+    margin: 0;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 48px;
+    line-height: 58px;
+}
+</style>
---
+++ src/components/Sections.vue
@@ -0,0 +1,29 @@
+<script>
+import Section from '@/components/Section.vue';
+
+export default {
+    name: 'Sections',
+    components: {
+        Section
+    }
+}
+</script>
+
+<template>
+    <div class="sections">
+        <Section section="Categorías" url="#/categorias"/>
+        <Section section="Restaurantes" url="#/restaurantes"/>
+    </div>
+</template>
+
+<style scoped>
+.sections {
+    height: 375px;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 20px;
+    margin: 20px 0;
+}
+</style>
