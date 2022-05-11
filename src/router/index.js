a98f9acf5955fed48644afb44e1eff5337fcb9d4
josephLSalgado <josephLSalgado@outlook.com>
2022-05-10 19:16:21 -0500

    update: Router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,7 @@
-import { createRouter, createWebHashHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import { createRouter, createWebHashHistory } from 'vue-router';
+import HomeView from '../views/HomeView.vue';
+import CategoriesView from '@/views/CategoriesView.vue';
+import RestaurantsView from '@/views/RestaurantsView.vue';
 
 const routes = [
   {
@@ -8,12 +10,14 @@ const routes = [
     component: HomeView
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/categorias',
+    name: 'categories',
+    component: CategoriesView
+  },
+  {
+    path: '/restaurants',
+    name: 'restaurants',
+    component: RestaurantsView
   }
 ]
 
