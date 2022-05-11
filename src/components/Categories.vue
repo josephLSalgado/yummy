3df0cbfb8f637e920a6287af4d9929cd2dd64127
josephLSalgado <josephLSalgado@outlook.com>
2022-05-10 19:07:24 -0500

    add: Category componets

---
+++ src/components/Categories.vue
@@ -0,0 +1,44 @@
+<script>
+import Category from '@/components/Category.vue';
+
+export default {
+    name: 'Categories',
+    components: {
+        Category
+    }
+}
+</script>
+
+<template>
+    <div class="categories">
+        <!-- Renderizado de una lista -->
+        <ul class="categories-list">
+            <li v-for="(categories, index) in $store.getters.getCategories" 
+            :key="index">
+            <Category :category="categories.name" :src_image="categories.image"/>
+        </li>
+        </ul>
+    </div>
+</template>
+
+<style scoped>
+.categories {
+    height: 350px;
+    margin: 30px 0;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    align-items: center;
+    gap: 50px;
+}
+
+ul {
+    margin: 0;
+    padding: 0;
+    list-style-type: none;
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: center;
+    gap: 50px;
+}
+</style>
---
+++ src/components/Category.vue
@@ -0,0 +1,61 @@
+<script>
+export default {
+    name: 'Category',
+    props: ['category', 'src_image']
+}
+</script>
+
+<template>
+    <!-- Llama la función cuando se entra al div y le enviamos la variable 
+        category -->
+    <div class="category" @click="$store.dispatch('textToSpeech', category)">
+        <div class="container-image">
+            <div class="image">
+                <img :src="src_image" :alt="category">
+            </div>
+        </div>
+        <div class="container-text">
+            <h2>{{ category }}</h2>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.category {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    width: fit-content;
+    height: fit-content;
+}
+
+.container-image {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 250px;
+    height: 250px;
+    background: #C8D96F;
+    border-radius: 20px;
+}
+
+.image {
+    width: 200px;
+    height: 200px;
+    background: #FFFFFF;
+    border-radius: 20px;
+}
+
+img {
+    width: 100%;
+    height: 100%;
+}
+
+h2 {
+    margin: 0;
+    font-style: normal;
+    font-weight: 800;
+    font-size: 36px;
+    line-height: 44px;
+}
+</style>
